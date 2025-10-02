8ac66cb64c9892bd584c68f21221b5eeb62abd5e
Andrej Žabka <257720@vutbr.cz>
2025-10-02 11:36:49 +0200

    Text edited - one line

--- src/pages/project/how/HowLanding/HeroSection.tsx
+++ src/pages/project/how/HowLanding/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
             className="text-base sm:text-lg font-semibold leading-[140%] text-black/50 mb-2 sm:mb-4"
             style={{ fontFamily: 'Urbanist, sans-serif' }}
           >
-            Why
+            What
           </h2>
           
           {/* Main heading */}
