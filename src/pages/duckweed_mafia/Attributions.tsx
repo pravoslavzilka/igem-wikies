ae128afef2edea44eba34af808f18482545e83f2
pravoslavzilka <pravoslav.zilka@gmail.com>
2025-10-07 19:50:49 +0200

    Prelinks updated

--- src/pages/duckweed_mafia/Attributions.tsx
+++ src/pages/duckweed_mafia/Attributions.tsx
@@ -8,15 +8,15 @@ const Attributions = () => {
     
   return (
     <div className="max-w-6xl mx-auto">   
-        <h1 className="text-4xl text-center font-bold mb-4 mt-16" style={{ fontFamily: 'Space Grotesk, sans-serif' }}>Attributions</h1>
+        <h1 className="text-4xl text-center font-bold mb-16 mt-16" style={{ fontFamily: 'Space Grotesk, sans-serif' }}>Attributions</h1>
 
 
 
         
         <iframe
-          style={{ width: "100%" }}
+          style={{ width: "100%", height: "1000px", border: "none" }}
           id="igem-attribution-form"
-          src={`https://teams.igem.org/wiki/${teamID}/attributions`}
+          src={`https://teams.igem.org/wiki/5642/attributions`}
         />
 
 
